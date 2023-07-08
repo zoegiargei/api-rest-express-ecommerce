694feceecbdc0e882696f301bc73099a2bea05a5
Zoe <zoegiargei00@gmail.com>
2023-07-08 17:33:23 -0300

    fix: Fix method authByApi

--- src/middlewares/authentication/jwt/auth.byJwt.api.js
+++ src/middlewares/authentication/jwt/auth.byJwt.api.js
@@ -1,8 +1,9 @@
 import passport from 'passport'
+import errors from '../../../lib/customErrors.js'
 
 export function authJwtApi (req, res, next) {
     passport.authenticate('jwt', (error, user) => {
-        if (error || !user) return next(error)
+        if (error || !user) return next(errors.authentication_failed.withDetails('You are not logged in'))
         const securityUser = { ...JSON.parse(user.payload), password: '' }
         req.user = securityUser
         next()
