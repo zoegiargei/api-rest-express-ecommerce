78421535deb0f01ef6b80dade63d5d38e3e3bdab
Zoe <zoegiargei00@gmail.com>
2023-07-24 19:52:23 -0300

    build: Add DOMAIN environment variable

--- src/controllers/web/products.controller.web.js
+++ src/controllers/web/products.controller.web.js
@@ -1,5 +1,4 @@
 /* eslint-disable dot-notation */
-import { PORT } from '../../configs/server.config.js'
 import productServices from '../../services/product.services.js'
 
 export async function handlerShowProducts (req, res, next) {
@@ -8,8 +7,8 @@ export async function handlerShowProducts (req, res, next) {
         const limitValue = 10
         const allProducts = await productServices.productsByPaginate(limitValue, page)
         const thIsProd = allProducts['docs'].length > 0
-        const prevLink = allProducts.hasPrevPage ? `http://localhost:${PORT}/web/home?page=${allProducts.prevPage}` : null
-        const nextLink = allProducts.hasNextPage ? `http://localhost:${PORT}/web/home?page=${allProducts.nextPage}` : null
+        const prevLink = allProducts.hasPrevPage ? `${process.env.DOMAIN}/web/home?page=${allProducts.prevPage}` : null
+        const nextLink = allProducts.hasNextPage ? `${process.env.DOMAIN}/web/home?page=${allProducts.nextPage}` : null
         const numPage = allProducts.page
         const user = req.user
         let dataCart
@@ -43,8 +42,8 @@ export async function handlerShowProductsByCategory (req, res, next) {
         const allProducts = await productServices.productsByCategory(clearCategory, limitValue, page)
         console.log(allProducts['docs'])
         const thIsProd = allProducts['docs'].length > 0
-        const prevLink = allProducts.hasPrevPage ? `http://localhost:${PORT}/web/home?page=${allProducts.prevPage}` : null
-        const nextLink = allProducts.hasNextPage ? `http://localhost:${PORT}/web/home?page=${allProducts.nextPage}` : null
+        const prevLink = allProducts.hasPrevPage ? `${process.env.DOMAIN}/web/home?page=${allProducts.prevPage}` : null
+        const nextLink = allProducts.hasNextPage ? `${process.env.DOMAIN}/web/home?page=${allProducts.nextPage}` : null
         const numPage = allProducts.page
         const user = req.user
         let dataCart
